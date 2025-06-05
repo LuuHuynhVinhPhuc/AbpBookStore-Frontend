6e25009976a2cda70586d5cc9d7228d0f431e694
LuuHuynhVinhPhuc <vinhphucluuhuynh@gmail.com>
2025-06-05 10:39:55 +0700

    feat: first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: true,
+  application: {
+    baseUrl,
+    name: 'Default_Template',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44332/',
+    redirectUri: baseUrl,
+    clientId: 'Default_Template_App',
+    responseType: 'code',
+    scope: 'offline_access Default_Template',
+    requireHttps: true,
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44332',
+      rootNamespace: 'Default_Template',
+    },
+  },
+} as Environment;
